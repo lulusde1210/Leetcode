85d6148fe9fafc84d7c2c032458219e8b2340563
Lu <114371764+lulusde1210@users.noreply.github.com>
2023-10-16 22:44:40 -0500

    Time: 70 ms (33.17%), Space: 16.4 MB (27.81%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,16 +1,38 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-         longest = 0
-         i,j = 0,0
-         hash = {}
 
-         while j < len(s):
-             if s[j] in hash and hash[s[j]] >= i:
-                 i = hash[s[j]] + 1
+##########use a hash set#################
+        letter_set = set()
+        i,j = 0,0
+        longest = 0
 
-             longest = max(longest, len(s[i:j+1]))
-             hash[s[j]] = j
-             j += 1
+        while j < len(s):
+            if s[j] not in letter_set:
+                longest = max(longest, len(s[i:j+1]))
+                letter_set.add(s[j])
+                j+=1
+            else:
+                while s[j] in letter_set:
+                    letter_set.remove(s[i])
+                    i += 1
+        
+        return longest
 
-         return longest
+
+
+
+###########use a hash map#################
+        #  longest = 0
+        #  i,j = 0,0
+        #  hash = {}
+
+        #  while j < len(s):
+        #      if s[j] in hash and hash[s[j]] >= i:
+        #          i = hash[s[j]] + 1
+
+        #      longest = max(longest, len(s[i:j+1]))
+        #      hash[s[j]] = j
+        #      j += 1
+
+        #  return longest
 
